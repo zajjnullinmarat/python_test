6e89a24743d4ead8079dfae2db933a40a5a1f981
marat <proxyserver@rambler.ru>
2023-02-11 23:38:55 +0300

    pervyi nah

--- start.py
+++ start.py
@@ -1,6 +1,6 @@
-def check_duplicate(lst):
+def check_duplicates(lst):
     return len(lst) != len(set(lst))
-print(check_duplicate([1,2,3,4,5,4,6])) # True
-print(check_duplicate([1,2,3])) # False
-print(check_duplicate([1,2,3,4,9])) # False
+print(check_duplicates([1,2,3,4,5,4,6])) # True
+print(check_duplicates([1,2,3])) # False
+print(check_duplicates([1,2,3,4,9])) # False
 print("success")

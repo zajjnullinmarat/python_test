eacde8e2a1f196c3837ea90e907980a8edcac3c9
marat <proxyserver@rambler.ru>
2023-02-12 00:46:06 +0300

    1
    2
    
    3

--- start.py
+++ start.py
@@ -4,10 +4,4 @@ print(check_duplicated([1,2,3,4,5,4,6])) # True
 print(check_duplicated([1,2,3])) # False
 print(check_duplicated([1,2,3,4,9])) # False
 
-def check_duplicated(lst):
-    return len(lst) != len(set(lst))
-print(check_duplicated([1,2,3,4,5,4,6])) # True
-print(check_duplicated([1,2,3])) # False
-print(check_duplicated([1,2,3,4,9])) # False
-
 print("success")
